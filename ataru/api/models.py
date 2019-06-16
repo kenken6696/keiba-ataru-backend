a591e444065dc1d02561bb1003340bc2fae598ad
skyeanka <zyunkoukan@gmail.com>
2019-06-16 20:33:50 +0000

    remodel project

--- ataru/api/models.py
+++ ataru/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Memo(models.Model):
+    title = models.CharField(max_length=64)
+    memo = models.TextField(max_length=1024)
+
+    def __str__(self):
+        return self.title
