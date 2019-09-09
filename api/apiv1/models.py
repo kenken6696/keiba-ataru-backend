b08c4cc74c2a86a45176f3bacb6e1adf2ab5ddd4
ren <zyunkoukan@gmail.com>
2019-09-09 12:12:18 +0900

    model作成

--- api/apiv1/models.py
+++ api/apiv1/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 # Write Model and Bussiness Logic
-class Race_Prediction(models.Model):
+class Race(models.Model):
+    id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(verbose_name='レース名', max_length=100)
+    date = models.DateField(verbose_name='日時')
+    racecourse = models.CharField(verbose_name='競馬場', max_length=100)
 
-    race_name = models.CharField(verbose_name='レース名', max_length=100)
-    
+
+class Horse(models.Model):
+    id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(verbose_name='馬名', max_length=100)
+
+class Race_Prediction(models.Model):
+    race_id = models.ManyToManyField(Race, verbose_name='レースID')
+    horse_id
+    trio_pred = models.IntegerField(verbose_name='三連複予想')
