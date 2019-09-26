904e233813d78362201ab45a793c109b5896cefa
skyeanka <zyunkoukan@gmail.com>
2019-09-26 21:46:20 +0900

    [WIP] add model

--- api/apiv1/models.py
+++ api/apiv1/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
-# Write Model and Bussiness Logic
-class Race(models.Model):
-    id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(verbose_name='レース名', max_length=100)
-    date = models.DateField(verbose_name='日時')
+# Write Model
+class RaceSet(models.Model):
+    race_set_id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    race_set_name = models.CharField(verbose_name='競走名', max_length=100)
+    date = models.DateField(verbose_name='実施日')
     racecourse = models.CharField(verbose_name='競馬場', max_length=100)
+
+class Race(models.Model):
+    race_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    race_name = models.CharField(verbose_name='レース名', max_length=100)
+    round = models.IntegerField(verbose_name='ラウンド')
+    race_starttime = models.DateField(verbose_name='レース開始時間')
 
 
 class Horse(models.Model):
-    id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(verbose_name='馬名', max_length=100)
-
-class Race_Prediction(models.Model):
-    race_id = models.ManyToManyField(Race, verbose_name='レースID')
-    horse_id
+    race_id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    horse_number = models.IntegerField(verbose_name='馬順')
+    horse_name = models.CharField(verbose_name='馬名', max_length=100)
     trio_pred = models.IntegerField(verbose_name='三連複予想')
