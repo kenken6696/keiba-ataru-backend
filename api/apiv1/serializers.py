615253cdab4ccc6884da5e2a9756b1b7b4540c8b
ren <zyunkoukan@gmail.com>
2019-09-27 21:11:32 +0900

    add GETs API

--- api/apiv1/serializers.py
+++ api/apiv1/serializers.py
@@ -4,14 +4,14 @@ from .models import RaceSet, Race, Horse
 class RaceSetSerializer(serializers.ModelSerializer):
     class Meta:
         model = RaceSet
-        field =  ('id', 'raceset_name', 'date', 'racecourse')
+        fields =  ('id', 'raceset_name', 'date', 'racecourse')
 
 class RaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = RaceSet
-        field =  ('id', 'race_name', 'round_number', 'starttime', 'raceset_name')
+        fields =  ('id', 'race_name', 'round_number', 'starttime', 'raceset_name')
 
 class HorseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Horse
-        field =  ('id', 'race_name', 'horse_number', 'horse_name', 'trio_pred')
+        fields =  ('id', 'race_name', 'horse_number', 'horse_name', 'trio_pred')
