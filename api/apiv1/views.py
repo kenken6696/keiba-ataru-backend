3a061ef4da2f5e11faa5493e81eec97385faa812
ren <zyunkoukan@gmail.com>
2019-09-30 08:17:16 +0900

    racesets/:GET req_dateの今週分返すよう実装

--- api/apiv1/views.py
+++ api/apiv1/views.py
@@ -3,21 +3,24 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from .serializers import RaceSetSerializer, RaceSerializer, HorseSerializer
 from .models import RaceSet, Race, Horse
-
+import datetime
 # Create your views here.
+
 class RaceSetListAPIView(generics.ListAPIView):
-    queryset = RaceSet.objects.all()
     serializer_class = RaceSetSerializer
-    def get_queryset(self):
-        return Race.objects.filter(raceset_name=self.kwargs['date'])
     
     def get_same_week_range(self, date):
-        req_date = date.today()
-        dist_from_next_sunday = 6 - req_date.weekday()
-        monday_date_on_same_week = req_date - (6 - dist_from_next_sunday)
-        sunday_date_on_same_week = req_date + dist_from_next_sunday
+        req_date = datetime.datetime.strptime(date, "%Y-%m-%d")
+        dist_from_monday = req_date.weekday()
+        dist_from_next_sunday = 6 - dist_from_monday
+        monday_date_on_same_week = req_date - datetime.timedelta(days=dist_from_monday)
+        sunday_date_on_same_week = req_date + datetime.timedelta(days=dist_from_next_sunday)
         return  monday_date_on_same_week, sunday_date_on_same_week
-
+    
+    def get_queryset(self):
+        req_date = self.request.GET.get('date_for_week_filter')
+        racesets = RaceSet.objects.filter(date__range = self.get_same_week_range(req_date))
+        return racesets
 
 class RaceListWithHorseAPIView(generics.ListAPIView):
     serializer_class = RaceSerializer
