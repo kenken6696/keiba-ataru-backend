c9fd405d016ffdf2189c497fc0254f644d69c511
skyeanka <zyunkoukan@gmail.com>
2019-10-04 01:02:45 +0900

    drf-yasgでschema生成していく

--- api/config/urls.py
+++ api/config/urls.py
@@ -31,6 +31,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger.yaml', schema_view.without_ui(cache_timeout=0)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('api/v1/', include('apiv1.urls')),
 ]
