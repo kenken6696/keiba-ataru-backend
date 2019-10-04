3e54cdc6823ee4026d6f80e3c900ff7639b7daab
ken <zyunkoukan@gmail.com>
2019-10-04 15:59:08 +0900

    get_schema_viewでdescription取れるようにコメント修正

--- api/apiv1/views.py
+++ api/apiv1/views.py
@@ -6,9 +6,20 @@ from logzero import logger
 import datetime
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg.inspectors import SwaggerAutoSchema
 
 class RaceSetListAPIView(generics.ListCreateAPIView):
+    """
+    classdoc
+    
+    get:
+    date_for_week_filterと同週のracesets(競争名)のリストを取得する
+    
+    post:
+    crawl_and_pred_flagが1の場合、今週のracesets以下を取得し、モデルより予測した結果をDB保存する
+    """
     serializer_class = RaceSetSerializer
+    swagger_schema = SwaggerAutoSchema
     
     def get_same_week_range(self, date):
         req_date = datetime.datetime.strptime(date, "%Y-%m-%d")
@@ -19,13 +30,18 @@ class RaceSetListAPIView(generics.ListCreateAPIView):
         return  monday_date_on_same_week, sunday_date_on_same_week
     
     def get_queryset(self):
+        # need this when using dynamically request parameters in view
+        if getattr(self, 'swagger_fake_view', False):
+        # queryset just for schema generation metadata
+            return Race.objects.none()
+        
         date_for_week_filter = self.request.query_params.get('date_for_week_filter')
         if date_for_week_filter is None:
             raise serializers.ValidationError('date_for_week_filter parameter is required, like 2019-06-21')
         racesets = RaceSet.objects.filter(date__range = self.get_same_week_range(date_for_week_filter))
         return racesets
 
-    date_for_week_filter = openapi.Parameter('date_for_week_filter', openapi.IN_QUERY, description="週間検索用の日にち", type=openapi.FORMAT_DATE)
+    date_for_week_filter = openapi.Parameter(name='date_for_week_filter', in_=openapi.IN_QUERY, description="週間検索用の日にち", type=openapi.FORMAT_DATE)
     @swagger_auto_schema(description='検索日時と同週の競走のリストを取得', manual_parameters=[date_for_week_filter])
     def list(self, request, *args, **kwargs):
 
@@ -44,6 +60,22 @@ class RaceSetListAPIView(generics.ListCreateAPIView):
         return response
 
 class RaceListWithHorseAPIView(generics.ListAPIView):
+    """
+    classdoc
+    
+    get:
+    racesets(競走名)に属するraceのリストを取得する
+    """
     serializer_class = RaceSerializer
     def get_queryset(self):
+        # need this when using dynamically request parameters in view
+        if getattr(self, 'swagger_fake_view', False):
+        # queryset just for schema generation metadata
+            return Race.objects.none()
         return Race.objects.filter(raceset_name=self.kwargs['pk'])
+    
+    @swagger_auto_schema(description='検索日時と同週の競走のリストを取得')
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        logger.info('id={}のracesetが照会された'.format(request.kwargs['pk']))
+        return response
